13c64616de4aa07cb4d934f78c03e37e0442e070
Khairiah Mohammed <92297044+Khiriah@users.noreply.github.com>
2022-10-26 12:09:09 +0300

    v2

---
+++ lib/eltezamat_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EltezamtScreen extends StatefulWidget {
+  const EltezamtScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EltezamtScreen> createState() => _EltezamtScreenState();
+}
+
+class _EltezamtScreenState extends State<EltezamtScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/masrofat_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Masrofat extends StatefulWidget {
+  const Masrofat({Key? key}) : super(key: key);
+
+  @override
+  State<Masrofat> createState() => _MasrofatState();
+}
+
+class _MasrofatState extends State<Masrofat> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
